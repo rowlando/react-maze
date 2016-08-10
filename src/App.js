8be179f66fc2b3cd5137089e447a278c011a5bfd
pco28 <pierre.coetzee@lad-tech.com>
2016-08-10 14:48:09 +0100

    switched to svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import createMaze from './Maze';
 
@@ -10,10 +9,6 @@ class App extends Component {
 
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
 
         <Maze size={10}/>
 
--- src/MazeTile.js
+++ src/MazeTile.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class MazeTile extends Component {
   render() {
     return (
-      <div className='mazetile'></div>
+      <svg width="120" height="120"
+     viewBox="0 0 120 120"
+     xmlns="http://www.w3.org/2000/svg">
+      <rect x="0" y="0" width="120" height="120" stroke="black" fill="transparent" stroke-width="1" />
+    </svg>
     )
   }
 }
